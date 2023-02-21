119cea5ef60b8e69e734dc58b39f87dd808c5ff9
Rubén Cuervo Madrigal <jrmcuervom@gmail.com>
2023-02-21 09:02:35 -0600

    Build html file and add functionallity

---
+++ app.js
@@ -0,0 +1,45 @@
+const books = JSON.parse(localStorage.getItem('books')) || [];
+
+function addBook(title, author) {
+    const book = { title, author };
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+    displayBooks();
+}
+
+function removeBook(title) {
+    const newBooks = books.filter(book => book.title !== title);
+    localStorage.setItem('books', JSON.stringify(newBooks));
+    books.length = 0;
+    books.push(...newBooks);
+    displayBooks();
+}
+
+function displayBooks() {
+    const booksList = document.getElementById('books-list');
+    booksList.innerHTML = '';
+    books.forEach(book => {
+        const li = document.createElement('li');
+        li.innerText = `${book.title} by ${book.author}`;
+        const removeButton = document.createElement('button');
+        removeButton.innerText = 'Remove';
+        removeButton.addEventListener('click', () => {
+            removeBook(book.title);
+        });
+        li.appendChild(removeButton);
+        booksList.appendChild(li);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    displayBooks();
+
+    const form = document.getElementById('add-book-form');
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        const title = document.getElementById('title').value;
+        const author = document.getElementById('author').value;
+        addBook(title, author);
+        form.reset();
+    });
+});
