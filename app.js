640586b10f831c301b60985023217e758b04c584
Rubén Cuervo Madrigal <jrmcuervom@gmail.com>
2023-02-21 10:19:18 -0600

    Debug the project

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 const books = JSON.parse(localStorage.getItem('books')) || [];
 
+function displayBooks() {
+  const booksList = document.getElementById('books-list');
+  booksList.innerHTML = '';
+  books.forEach((book) => {
+    const li = document.createElement('li');
+    li.innerHTML = `<div>${book.title}</div> <div>${book.author}</div>`;
+    const removeButton = document.createElement('button');
+    removeButton.innerHTML = 'Remove';
+    removeButton.addEventListener('click', () => {
+      removeBook(book.title); // eslint-disable-line no-use-before-define
+    });
+    li.appendChild(removeButton);
+    booksList.appendChild(li);
+    li.style.listStyleType = 'none';
+    const hr = document.createElement('hr');
+    booksList.appendChild(hr);
+  });
+}
+
 function addBook(title, author) {
-    const book = { title, author };
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-    displayBooks();
+  const book = { title, author };
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+  displayBooks();
 }
 
 function removeBook(title) {
-    const newBooks = books.filter(book => book.title !== title);
-    localStorage.setItem('books', JSON.stringify(newBooks));
-    books.length = 0;
-    books.push(...newBooks);
-    displayBooks();
-}
-
-function displayBooks() {
-    const booksList = document.getElementById('books-list');
-    booksList.innerHTML = '';
-    books.forEach(book => {
-        const li = document.createElement('li');
-        li.innerHTML = `<div>${book.title}</div> <div>${book.author}</div>`;
-        const removeButton = document.createElement('button');
-        removeButton.innerHTML = 'Remove';
-        removeButton.addEventListener('click', () => {
-            removeBook(book.title);
-        });
-        li.appendChild(removeButton);
-        booksList.appendChild(li);
-        li.style.listStyleType = 'none';
-        const hr = document.createElement('hr');
-        booksList.appendChild(hr);
-    });
+  const newBooks = books.filter((book) => book.title !== title);
+  localStorage.setItem('books', JSON.stringify(newBooks));
+  books.length = 0;
+  books.push(...newBooks);
+  displayBooks();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    displayBooks();
+  displayBooks();
 
-    const form = document.getElementById('add-book-form');
-    form.addEventListener('submit', event => {
-        event.preventDefault();
-        const title = document.getElementById('title').value;
-        const author = document.getElementById('author').value;
-        addBook(title, author);
-        form.reset();
-    });
+  const form = document.getElementById('add-book-form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    addBook(title, author);
+    form.reset();
+  });
 });
