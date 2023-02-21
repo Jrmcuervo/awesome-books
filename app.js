798df57131d6053b077904dc44bd503f29379428
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-21 23:04:28 +0430

    Update app.js for changing to class structure

--- app.js
+++ app.js
@@ -1,48 +1,53 @@
-const books = JSON.parse(localStorage.getItem('books')) || [];
-
-function displayBooks() {
-  const booksList = document.getElementById('books-list');
-  booksList.innerHTML = '';
-  books.forEach((book) => {
-    const li = document.createElement('li');
-    li.innerHTML = `<div>${book.title}</div> <div>${book.author}</div>`;
-    const removeButton = document.createElement('button');
-    removeButton.innerHTML = 'Remove';
-    removeButton.addEventListener('click', () => {
-      removeBook(book.title); // eslint-disable-line no-use-before-define
-    });
-    li.appendChild(removeButton);
-    booksList.appendChild(li);
-    li.style.listStyleType = 'none';
-    const hr = document.createElement('hr');
-    booksList.appendChild(hr);
-  });
+class Books{
+    constructor(){
+        this.books=JSON.parse(localStorage.getItem('books')) || [];
+    }
+     displayBooks() {
+      const booksList = document.getElementById('books-list');
+      booksList.innerHTML = '';
+      this.books.forEach((book) => {
+        const li = document.createElement('li');
+        li.innerHTML = `<div>${book.title}</div> <div>${book.author}</div>`;
+        const removeButton = document.createElement('button');
+        removeButton.innerHTML = 'Remove';
+        removeButton.addEventListener('click', () => {
+          this.removeBook(book.title); // eslint-disable-line no-use-before-define
+        });
+        
+        li.appendChild(removeButton);
+        booksList.appendChild(li);
+        li.style.listStyleType = 'none';
+        const hr = document.createElement('hr');
+        booksList.appendChild(hr);
+      });
+    }
+    
+     addBook(title, author) {
+      const book = { title, author };
+      this.books.push(book);
+      localStorage.setItem('books', JSON.stringify(this.books));
+      this.displayBooks();
+    }
+    
+     removeBook(title) {
+      const newBooks = this.books.filter((book) => book.title !== title);
+      localStorage.setItem('books', JSON.stringify(newBooks));
+      this.books.length = 0;
+      this.books.push(...newBooks);
+      this.displayBooks();
+    }
 }
 
-function addBook(title, author) {
-  const book = { title, author };
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-  displayBooks();
-}
-
-function removeBook(title) {
-  const newBooks = books.filter((book) => book.title !== title);
-  localStorage.setItem('books', JSON.stringify(newBooks));
-  books.length = 0;
-  books.push(...newBooks);
-  displayBooks();
-}
 
 document.addEventListener('DOMContentLoaded', () => {
-  displayBooks();
-
+    let books=new Books();
+    books.displayBooks();
   const form = document.getElementById('add-book-form');
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
-    addBook(title, author);
+    books.addBook(title, author);
     form.reset();
   });
 });
