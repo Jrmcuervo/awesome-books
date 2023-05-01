17d0cb07cb0a1b37549208166a2806fc4c67d9b1
Jrmcuervo <jrmcuervom@gmail.com>
2023-05-01 09:59:25 -0600

    fix linters

--- modules/nav.js
+++ modules/nav.js
@@ -6,21 +6,21 @@ const addANewSection = document.querySelector('.add-a-new-book');
 const contactSection = document.querySelector('.contact');
 
 navList.addEventListener('click', () => {
-    listSection.classList.remove('hide');
-    addANewSection.classList.add('hide');
-    contactSection.classList.add('hide');
+  listSection.classList.remove('hide');
+  addANewSection.classList.add('hide');
+  contactSection.classList.add('hide');
 });
 
 navAdd.addEventListener('click', () => {
-    listSection.classList.add('hide');
-    addANewSection.classList.remove('hide');
-    contactSection.classList.add('hide');
+  listSection.classList.add('hide');
+  addANewSection.classList.remove('hide');
+  contactSection.classList.add('hide');
 });
 
 navContact.addEventListener('click', () => {
-    listSection.classList.add('hide');
-    addANewSection.classList.add('hide');
-    contactSection.classList.remove('hide');
+  listSection.classList.add('hide');
+  addANewSection.classList.add('hide');
+  contactSection.classList.remove('hide');
 });
 
 export default function setupNavigation() {
